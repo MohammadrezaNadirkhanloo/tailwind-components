ca25a9b943afe1fc6df7376f17f0e434bc1c39e2
khanloo <m.nadirkhanloo1380@gmail.com>
2024-05-04 11:07:48 +0330

    add pro

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    container: {
+      center: true,
+      padding:"1rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
